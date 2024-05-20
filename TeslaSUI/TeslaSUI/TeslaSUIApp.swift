d2a5f7969eb7d43c54f3c29ffb737529fc894b4c
devinigor <devinigor@icloud.com>
2024-05-20 12:54:50 +0300

    new

---
+++ TeslaSUI/TeslaSUI/TeslaSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeslaSUIApp.swift
+//  TeslaSUI
+//
+//  Created by Игорь Девин on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TeslaSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
